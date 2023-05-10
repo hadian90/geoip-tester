f0f8ca511dc0436660d23177b56e8f5ac50b3b75
Abdul Hadi <hadian90@gmail.com>
2023-05-10 16:24:10 +0800

    first commit

---
+++ getLocationByIP.php
@@ -0,0 +1,24 @@
+<?php
+    require_once 'vendor/autoload.php';
+    use GeoIp2\Database\Reader;
+
+    $reader = new Reader('/usr/share/GeoIP/GeoLite2-Country.mmdb');
+
+    $ip_list = array(
+        '8.8.8.8',
+        '1.1.1.1',
+        '139.178.84.217',
+    );
+    foreach ($ip_list as $ip){
+        $record = $reader->city($ip);
+
+        print("Information for IP: $ip\n");
+
+        print("Country Short Code: " . $record->country->isoCode . "\n");
+        print("Country Name: " . $record->country->name . "\n");
+
+        print("Latitude: ". $record->location->latitude ." \n");
+        print("Longitude: ". $record->location->longitude ."\n");
+        print ("====================================\n");
+    }
+?>
