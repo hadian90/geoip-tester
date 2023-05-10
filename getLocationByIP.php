dc41bfe706652f37b95def3fff5a00706b31136c
Abdul Hadi <hadian90@gmail.com>
2023-05-10 16:34:45 +0800

    add test ip

--- getLocationByIP.php
+++ getLocationByIP.php
@@ -5,8 +5,8 @@
     $reader = new Reader('/usr/share/GeoIP/GeoLite2-Country.mmdb');
 
     $ip_list = array(
-        '8.8.8.8',
-        '1.1.1.1',
+        '147.158.176.40',
+        '185.203.122.19',
         '139.178.84.217',
     );
     foreach ($ip_list as $ip){
